b42a424954d3b4b7a0dac2ed71e73c2f4c99b80b
Abhimanyu Chugh <abhimanyuchugh@users.noreply.github.com>
2017-06-26 01:09:01 +0100

    Add unit test for author deletion on a book

---
+++ BookDatabase.Tests/BookTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookDatabase.Tests
+{
+    [TestClass]
+    public class BookTest
+    {
+        [TestMethod]
+        public void TestDeleteAuthorThatDoesntExist()
+        {
+            var authors = new List<Author>()
+            {
+                new Author("AuthorA"),
+                new Author("AuthorB"),
+            };
+            var book = new Book("BookTitle", authors);
+            book.DeleteAuthor(authors[0].Name + "1111");
+            Assert.AreEqual(2, book.Authors.Count);
+            CollectionAssert.AreEquivalent(authors, book.Authors.ToList());
+        }
+
+        [TestMethod]
+        public void TestDeleteAuthor()
+        {
+            var authorA = new Author("AuthorA");
+            var authorB = new Author("AuthorB");
+            var authors = new List<Author>()
+            {
+                authorA,
+                authorB,
+            };
+            var book = new Book("BookTitle", authors);
+            book.DeleteAuthor(authorA.Name);
+            Assert.AreEqual(1, book.Authors.Count);
+            var expectedAuthors = new List<Author> { authorB };
+            CollectionAssert.AreEquivalent(expectedAuthors, book.Authors.ToList());
+        }
+    }
+}
